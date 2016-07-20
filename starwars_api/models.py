ec30b0ec3c939662942ab600bc2194f3fa1fcaec
Jessica Ngo <jessica.ngo@live.com>
2016-07-20 02:55:40 +0000

    Progress so far

--- starwars_api/models.py
+++ starwars_api/models.py
@@ -1,5 +1,7 @@
 from starwars_api.client import SWAPIClient
 from starwars_api.exceptions import SWAPIClientError
+#I am adding the line below
+
 
 api_client = SWAPIClient()
 
@@ -11,7 +13,19 @@ class BaseModel(object):
         Dynamically assign all attributes in `json_data` as instance
         attributes of the Model.
         """
-        pass
+        print json_data
+        for key, value in json_data.iteritems():
+            setattr(self, key, value)
+            
+            
+        
+        
+        #so if json_data = {'people': 'luke'}, this allows for BaseModel.people = 'luke'?
+        #Maybe I am not understanding then; are we creating a dictionary?
+        #In my mind, the two ways to approach this are to iterate through everything
+        # and pull what we need or make a way to reference back to the AGI every time
+        # my impression was the latter, but I make more than my share of mistakes
+        # i have absolutely no idea lol
 
     @classmethod
     def get(cls, resource_id):
@@ -19,7 +33,17 @@ class BaseModel(object):
         Returns an object of current Model requesting data to SWAPI using
         the api_client.
         """
-        pass
+        #gets called like luke= People.get(1)
+        #creates an object named luke with all attributes    
+        
+        method = "get_{}".format(cls.RESOURCE_NAME)
+        retrieved_data = getattr(api_client, method)(people_id = resource_id)
+        retrieved_data = BaseModel(retrieved_data)
+        # for key, value in retrieved_data:
+        #     setattr(self, key, value)
+        print retrieved_data
+        return retrieved_data #returns loaded python object from json file
+        
 
     @classmethod
     def all(cls):
@@ -28,8 +52,10 @@ class BaseModel(object):
         later in charge of performing requests to SWAPI for each of the
         pages while looping.
         """
-        pass
-
+        #call on base queryset...? instance an object
+        modelqueryset = "{}QuerySet".format(cls.__name__)
+        eval(modelqueryset)
+        
 
 class People(BaseModel):
     """Representing a single person"""
@@ -55,18 +81,48 @@ class Films(BaseModel):
 class BaseQuerySet(object):
 
     def __init__(self):
+        
         pass
+    
 
     def __iter__(self):
-        pass
+        #initializing 
+        
+        self.index = 0
+        self.current_page = 1
+        #self.total_page = 1
+        #loading in one page at a time, initialize first page
+        self.curr_page_results = getattr(api_client, method)(page = current_page)
+    
+        return self
 
     def __next__(self):
         """
         Must handle requests to next pages in SWAPI when objects in the current
         page were all consumed.
         """
-        pass
-
+        method = "get_{}".format(self.RESOURCE_NAME)
+        
+        #check pagelength
+        self.pagelength = len(self.results)
+        
+        #iterate through results on page
+        while self.index <= self.pagelength:
+            yield self.results[self.index]
+            self.index +=1
+            
+        #go to next page
+        self.current_page += 1
+        
+        #check for next page exists, if it does, reset index & load next page
+        if getattr(api_client, method)(page = self.current_page):
+            self.curr_page_results = getattr(api_client, method)(page = current_page)
+            self.index = 0
+            #self.total_page += 1
+            #theoretically can enter while loop again?
+        else: #page does not exist
+            raise StopIteration
+        
     next = __next__
 
     def count(self):
@@ -75,7 +131,14 @@ class BaseQuerySet(object):
         If the counter is not persisted as a QuerySet instance attr,
         a new request is performed to the API in order to get it.
         """
-        pass
+        if self.count:
+            return self.count #'count' key contains total entries
+        else:
+            method = "get_{}".format(self.RESOURCE_NAME)
+            retrieved_data = getattr(self, method)()
+            return retrieved_data["count"]
+            
+            #manually count
 
 
 class PeopleQuerySet(BaseQuerySet):
